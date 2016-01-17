2fbaba0f38f638bbc6668dfffb1693f02fa92395
spaghettisource <natureelement@gmail.com>
2016-01-17 02:34:09 -0800

    Initial commit

---
+++ GetScreenshot/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace GetScreenshot
+{
+    partial class Screnshot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Screnshot));
+            this.imgContainer = new System.Windows.Forms.PictureBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.imgContainer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgContainer
+            // 
+            this.imgContainer.Location = new System.Drawing.Point(13, 13);
+            this.imgContainer.Name = "imgContainer";
+            this.imgContainer.Size = new System.Drawing.Size(259, 236);
+            this.imgContainer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.imgContainer.TabIndex = 0;
+            this.imgContainer.TabStop = false;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // Screnshot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.imgContainer);
+            this.Name = "Screnshot";
+            this.Text = "Screenshot";
+            this.Resize += new System.EventHandler(this.Screnshot_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.imgContainer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgContainer;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ GetScreenshot/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace GetScreenshot
+{
+    public partial class Screnshot : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregiterRegisterHotKey(IntPtr hWnd, int id);
+
+        const int MYACTION_HOTKEY_ID = 1;
+
+        public Screnshot()
+        {
+            InitializeComponent();
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID, 0, (int)Keys.F11);
+        }
+
+        public System.Drawing.Image GetClipBoardImage()
+        {
+            System.Drawing.Image returnImage = null;
+            if (Clipboard.ContainsImage())
+            {
+                returnImage = Clipboard.GetImage();
+            }
+            return returnImage;
+        }
+
+        private void Screnshot_Resize(object sender, EventArgs e)
+        {
+            if(this.WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon.Visible = true;
+                notifyIcon.BalloonTipText = "Minimized to tray";
+                notifyIcon.BalloonTipIcon = ToolTipIcon.Info;
+                notifyIcon.ShowBalloonTip(100);                
+                this.Hide();
+            }
+
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                notifyIcon.Visible = false;                
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if(m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_ID)
+            {
+                SendKeys.SendWait("{PRTSC}");
+                imgContainer.Image = GetClipBoardImage();
+                try
+                {
+                    imgContainer.Image.Save("Image" + System.DateTime.UtcNow.ToFileTime() + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("There was an error saving the screenshot");
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Maximized;
+        }
+    }
+}
